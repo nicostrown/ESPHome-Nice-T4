acfbacb4f07e56946fc6d94e4e88b018f0e7e458
Nicolas Romana <nicolas@ribere.fr>
2024-09-14 22:06:21 +0200

    add address

--- components/nice_cover/cover.py
+++ components/nice_cover/cover.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import cover
 from esphome.components import uart
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID,CONF_ADDRESS
 
 DEPENDENCIES = ["uart"]
 
@@ -10,7 +10,8 @@ nice_cover_ns = cg.esphome_ns.namespace("nice_cover")
 NiceCover = nice_cover_ns.class_("NiceCover", cover.Cover, cg.Component, uart.UARTDevice)
 
 CONFIG_SCHEMA = cover.COVER_SCHEMA.extend(
-    {cv.GenerateID(): cv.declare_id(NiceCover)}
+    {cv.GenerateID(): cv.declare_id(NiceCover),
+     cv.Optional(CONF_USE_ADDRESS): cv.hex_uint16_t}
 ).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
 
@@ -19,3 +20,7 @@ async def to_code(config):
     await cg.register_component(var, config)
     await cover.register_cover(var, config)
     await uart.register_uart_device(var, config)
+
+    if CONF_ADDRESS in config:
+        address = config[CONF_ADDRESS]
+        cg.add(var.set_to_address(address))
